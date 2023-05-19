93fdab44eb193e9b0284835ce0f0b970dc33fa32
AmanPathak-DevOps <aman07pathak@gmail.com>
2023-05-19 13:58:07 +0530

    Minikube Installation Script Added

---
+++ Install-Minikube.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+sudo apt update
+sudo apt upgrade
+wget https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+chmod +x minikube-linux-amd64
+sudo mv minikube-linux-amd64 /usr/local/bin/minikube
