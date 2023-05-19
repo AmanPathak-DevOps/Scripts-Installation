d938d59e7a920001621200983f321d8c91f0e16e
AmanPathak-DevOps <aman07pathak@gmail.com>
2023-05-19 13:58:30 +0530

    Sonarqube Installation Script Added

---
+++ Install-Sonarqube.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+apt install unzip
+adduser sonarqube
+wget https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-9.4.0.54424.zip
+unzip *
+chmod -R 755 /home/sonarqube/sonarqube-9.4.0.54424
+chown -R sonarqube:sonarqube /home/sonarqube/sonarqube-9.4.0.54424
+cd sonarqube-9.4.0.54424/bin/linux-x86-64/
+./sonar.sh start
+
+
