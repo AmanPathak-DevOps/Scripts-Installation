0fe50a0cf3cda4c9294e5017bf78e6bc031deecc
Aman Pathak <85124250+AmanPathak-DevOps@users.noreply.github.com>
2023-12-27 19:45:31 +0530

    Update Install-Docker.sh

--- Install-Docker.sh
+++ Install-Docker.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
 sudo apt update
-sudo apt install docker.io
-sudo su -
-usermod -aG docker jenkins
-usermod -aG docker ubuntu
-systemctl restart docker
+sudo apt install docker.io -y
+sudo usermod -aG docker jenkins
+sudo usermod -aG docker ubuntu
+sudo systemctl restart docker
+sudo chmod 777 /var/run/docker.sock
