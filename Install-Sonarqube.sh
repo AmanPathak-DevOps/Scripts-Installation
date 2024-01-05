d92bf26c4c6d6da0f5f232e1479def6594d5172d
Aman Pathak <85124250+AmanPathak-DevOps@users.noreply.github.com>
2024-01-05 20:15:53 +0530

    Update Install-Sonarqube.sh

--- Install-Sonarqube.sh
+++ Install-Sonarqube.sh
@@ -3,10 +3,10 @@
 sudo apt install unzip
 adduser sonarqube
 wget https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-9.4.0.54424.zip
-unzip *
-chmod -R 755 /home/sonarqube/sonarqube-9.4.0.54424
-chown -R sonarqube:sonarqube /home/sonarqube/sonarqube-9.4.0.54424
+unzip sonarqube-9.4.0.54424.zip
+sudo chmod -R 755 /home/sonarqube/sonarqube-9.4.0.54424
+sudo chown -R sonarqube:sonarqube /home/sonarqube/sonarqube-9.4.0.54424
 cd sonarqube-9.4.0.54424/bin/linux-x86-64/
-./sonar.sh start
+sudo ./sonar.sh start
 
 
