29abd1ae6cfd201198efa9a3fe841f0030301bc1
Aman Pathak <85124250+AmanPathak-DevOps@users.noreply.github.com>
2024-01-05 18:14:28 +0530

    Create Install-kubectl.sh

---
+++ Install-kubectl.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+sudo apt update
+sudo apt install curl
+curl -LO https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+kubectl version --client
